df1fa97047f46c3e948aa28cc6ed2bd700b22d60
RUNLIN ZHOU <solostarwar@163.com>
2024-12-21 10:31:10 +0100

    Add files via upload

---
+++ MU5IN552-DAAR/TME8_webAPI/mySearchEngine/mySearchEngine/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mySearchEngine project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mySearchEngine.settings')
+
+application = get_asgi_application()
---
+++ MU5IN552-DAAR/TME8_webAPI/mySearchEngine/mytig/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from mytig import views
+
+urlpatterns = [
+    path('products/', views.RedirectionListeDeProduits.as_view()),
+    path('product/<int:pk>/', views.RedirectionDetailProduit.as_view()),
+    path('onsaleproducts/', views.PromoList.as_view()),
+    path('onsaleproduct/<int:pk>/', views.PromoDetail.as_view()),
+]
